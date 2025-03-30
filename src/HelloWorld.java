512121ea7dc3c6680f460e0cefbe042610f6288d
shiva <ss12@umbc.edu>
2025-03-30 15:42:56 -0400

    updated code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,7 @@ public class HelloWorld {
         System.out.println("Hello, World! This is shiva test");
         System.out.println("This is test");
         System.out.println("This is test 2");
+        Student mystudent=new Student();
+        System.out.println(mystudent.studentName);
     }
 }
